b192bab103caea8e8f03cf94976030132ad4894b
Vinicius Gomes <vvgomess@gmail.com>
2012-05-23 02:58:47 -0300

    the 100 sequence example automated

--- main.rb
+++ main.rb
@@ -1,4 +1,4 @@
 require File.expand_path(File.dirname(__FILE__) + '/config/environment')
 
-max = ARGV[0].to_i
-puts SequenceRenderer.new(NumberRenderer.new).render(max)
+max = ARGV[0] || 100
+puts SequenceRenderer.new(NumberRenderer.new).render(max.to_i)
--- spec/acceptance/acceptance_spec.rb
+++ spec/acceptance/acceptance_spec.rb
@@ -1,10 +1,16 @@
 describe 'A Ping Pong App user' do
 	
   MAIN = File.expand_path(File.dirname(__FILE__) + "/../../main.rb")
+  SAMPLE = File.expand_path(File.dirname(__FILE__) + "/../../sample_output.txt")
 
   it 'should see pings, pongs and a p*ng when providing "15" as input' do
     expected_stdout = "1, 2, ping, 4, pong, ping, 7, 8, ping, pong, 11, ping, 13, 14, p*ng\n"
     `ruby #{MAIN} 15`.should == expected_stdout
+  end
+
+  it 'should see a lot of pings, pongs, p*ngs for the default input' do
+  	expected_stdout = File.open(SAMPLE, 'r').read
+  	`ruby #{MAIN}`.should == expected_stdout
   end
 
 end
