2222e419a4a873b3f4e3b791e10c46696c92108a
Vinicius Gomes <vvgomess@gmail.com>
2012-05-23 03:35:49 -0300

    argument validation

--- main.rb
+++ main.rb
@@ -1,4 +1,10 @@
 require File.expand_path(File.dirname(__FILE__) + '/config/environment')
 
-max = ARGV[0] || 100
-puts SequenceRenderer.new(NumberRenderer.new).render(max.to_i)
+max = ARGV[0] ? ARGV[0].to_i : 100
+
+if max > 0
+  puts SequenceRenderer.new(NumberRenderer.new).render(max)
+else
+  puts 'If you provide an argument, it must be a number > 0.'
+end
+
--- spec/acceptance/acceptance_spec.rb
+++ spec/acceptance/acceptance_spec.rb
@@ -3,14 +3,19 @@ describe 'A Ping Pong App user' do
   MAIN = File.expand_path(File.dirname(__FILE__) + "/../../main.rb")
   SAMPLE = File.expand_path(File.dirname(__FILE__) + "/../../sample_output.txt")
 
-  it 'should see pings, pongs and a p*ng when providing "15" as input' do
+  it 'should see pings, pongs and a single p*ng when providing "15" as argument' do
     expected_stdout = "1, 2, ping, 4, pong, ping, 7, 8, ping, pong, 11, ping, 13, 14, p*ng\n"
     `ruby #{MAIN} 15`.should == expected_stdout
   end
 
-  it 'should see a lot of pings, pongs, p*ngs for the default input' do
+  it 'should see a lot of pings, pongs, p*ngs for the default argument' do
   	expected_stdout = File.open(SAMPLE, 'r').read
   	`ruby #{MAIN}`.should == expected_stdout
+  end
+
+  it 'should see an error message when providing an invalid argument' do
+  	expected_stdout = "If you provide an argument, it must be a number > 0.\n"
+    `ruby #{MAIN} foo`.should == expected_stdout
   end
 
 end
